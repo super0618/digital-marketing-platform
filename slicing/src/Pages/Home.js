6a0ba407f3e0af726104956cd12bdbb61c34c8ed
EDMPhoenix <greatm1257@gmail.com>
2024-01-05 20:21:14 -0500

    feat(slicing): Add carousel

--- slicing/src/Pages/Home.js
+++ slicing/src/Pages/Home.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Header from '../Components/Header';
+import MainCrousel from '../Components/Carousel';
 
 class Home extends Component {
   render() {
     return (
       <div className='wrapper'>
         <Header />
-        Body
+        <div className='main-carousel'>
+          <MainCrousel />
+        </div>
       </div>
     )
   }
